fc813fb24203ceda7b21f907bcd9abe1bec8d70d
Qasim Laghari <go.jets.laghari@gmail.com>
2019-08-25 19:31:06 -0700

    Add function for running knn model

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -2,6 +2,22 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 
+function knn(features, labels, predictionPoint, k) {
+  return (
+    features
+      .sub(predictionPoint)
+      .pow(2)
+      .sum(1)
+      .pow(0.5)
+      .expandDims(1)
+      .concat(labels, 1)
+      .unstack()
+      .sort((a, b) => (a.get(0) > b.get(0) ? 1 : -1))
+      .slice(0, k)
+      .reduce((acc, pair) => acc + pair.get(1), 0) / k
+  );
+}
+
 let {
   features,
   labels,
@@ -14,5 +30,8 @@ let {
   labelColumns: ['price']
 });
 
-console.log(testFeatures);
-console.log(testLabels);
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+
+const result = knn(features, labels, tf.tensor(testFeatures[0]), 10);
+console.log('Guess: ', result, testLabels[0][0]);
