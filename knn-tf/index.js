2da85f55b1e1d7bd67301d6d35916b3d0b4c2105
Qasim Laghari <go.jets.laghari@gmail.com>
2019-09-02 14:35:09 -0700

    Determine error range

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -34,4 +34,5 @@ features = tf.tensor(features);
 labels = tf.tensor(labels);
 
 const result = knn(features, labels, tf.tensor(testFeatures[0]), 10);
-console.log('Guess: ', result, testLabels[0][0]);
+const err = (testLabels[0][0] - result) / testLabels[0][0];
+console.log('Error', err * 100);
