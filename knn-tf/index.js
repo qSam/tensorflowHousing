ca5564dd5e764b3c842c8801963b8b162d2517f5
Qasim Laghari <go.jets.laghari@gmail.com>
2019-09-03 16:34:27 -0700

    Fetch more features to optimize output

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -30,7 +30,7 @@ let {
 } = loadCSV('kc_house_data.csv', {
   shuffle: true,
   splitTest: 10,
-  dataColumns: ['lat', 'long', 'sqft_lot'],
+  dataColumns: ['lat', 'long', 'sqft_lot', 'sqft_living'],
   labelColumns: ['price']
 });
 
