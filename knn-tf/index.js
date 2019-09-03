7fc57593bfd60165dab6498f3c8cbc692e0ba849
Qasim Laghari <go.jets.laghari@gmail.com>
2019-09-03 16:24:49 -0700

    Standadrize feature values

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -3,9 +3,13 @@ const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 
 function knn(features, labels, predictionPoint, k) {
+  const { mean, variance } = tf.moments(features, 0);
+  const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
   return (
     features
-      .sub(predictionPoint)
+      .sub(mean)
+      .div(variance.pow(0.5))
+      .sub(scaledPrediction)
       .pow(2)
       .sum(1)
       .pow(0.5)
@@ -26,7 +30,7 @@ let {
 } = loadCSV('kc_house_data.csv', {
   shuffle: true,
   splitTest: 10,
-  dataColumns: ['lat', 'long'],
+  dataColumns: ['lat', 'long', 'sqft_lot'],
   labelColumns: ['price']
 });
 
